5ffb4f706befc81390bac4c7e5486beb811dc59b
Reefs-Endeavour <106530827+Reefs-Endeavour@users.noreply.github.com>
2025-07-10 16:06:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "d6e97a3d-c31e-46d4-a7a5-6986d4cfa693"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstateyourname"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
